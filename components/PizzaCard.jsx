e7a5497a21dbb952efd78dc3e4777f77ead8904a
sreidnoe <stephaniereidnoe@gmail.com>
2022-05-12 22:10:00 -0500

    updates

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -2,20 +2,18 @@ import styles from "../styles/PizzaCard.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-const PizzaCard = ({pizza}) => {
+const PizzaCard = ({ pizza }) => {
   return (
     <div className={styles.container}>
       <div className={styles.pizza}>
         <Link href={`/product/${pizza._id}`}>
-        <Image src={pizza.img} alt="Pizza" width="400" height="400" />
+          <Image src={pizza.img} alt="Pizza" width="400" height="400" />
         </Link>
-        
       </div>
       <h1 className={styles.title}>{pizza.title}</h1>
-      <span className={styles.price}>{pizza.prices[0]}</span>
-      <p className={styles.desc}>
-        {pizza.desc}
-      </p>
+      <p className={styles.desc}>Starting At:</p>
+      <span className={styles.price}>${pizza.prices[0]}</span>
+      <p className={styles.desc}>{pizza.desc}</p>
     </div>
   );
 };
